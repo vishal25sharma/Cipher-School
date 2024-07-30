af33f2f8480725c5c4324cdf459080a9bf494609
vishal25sharma <122363289+vishal25sharma@users.noreply.github.com>
2024-07-30 21:32:05 +0530

    Create Lecture 11 - Cipher School.cpp

---
+++ Lecture 11 - Cipher School.cpp
@@ -0,0 +1,12 @@
+// in this lecture i have learnt about while loop
+#include <iostream>
+using namespace std;
+int main() {
+  int  i=1;//initialisation 
+  while(i<101){//loop
+      cout<<i<<" ";
+      i++; //update
+  }
+
+    return 0;
+}
