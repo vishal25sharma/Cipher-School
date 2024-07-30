f81dd3689725415c63f4b7094ec20baf019826b2
vishal25sharma <122363289+vishal25sharma@users.noreply.github.com>
2024-07-30 21:34:55 +0530

    Create Lecture 13 - Cipher School.cpp

---
+++ Lecture 13 - Cipher School.cpp
@@ -0,0 +1,10 @@
+//in this lecture i have learnt about functions and main function is explained about structure of function 
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    cin>>i;
+    cout<<i;
+
+    return 0;
+}
