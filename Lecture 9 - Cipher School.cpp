b94104d1c15d62c83355b36472bb5abbb8e41f4a
vishal25sharma <122363289+vishal25sharma@users.noreply.github.com>
2024-07-30 21:28:30 +0530

    Update Lecture 9 - Cipher School.cpp

--- Lecture 9 - Cipher School.cpp
+++ Lecture 9 - Cipher School.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
-	for (int i = 0;i<5;i++){   // By using the for loop print the numbers from 0 to 4.
-		cout<<i<<endl;
-	}
-	return 0;
-}
+int main() {
+    int a[5];
+    int i;
+    cout<<"Give 5 integers as input"<<endl;
+    for(i=0;i<5;i++){
+        cin>>a[i];
+    }
+    for(i=5;i>=0;i--){
+        cout<<a[i]<<" ";
+    }
 
-#include<iostream>
-using namespace std;
-int main(){
-	for(int i=0; i<=100;i++){ //here by using the for loop print 100 numbers
-		cout<<i<<endl;
-	}
-	return 0;
-}
-
-#include<iostream>
-using namespace std;
-int main(){
-	for(int i=0; i<100;i=i+2){ //here by using the for loop print the even numbers upto 100
-		cout<<i<<endl;
-	}
-	return 0;
+    return 0;
 }
